c9ad205ef28223e2ec303008cfdaf876e929c72b
Luna Vasconcelos <lunacv94@gmail.com>
2022-05-12 23:20:42 -0300

    Alteração nos campos da tabela de usuário e adicionado método post ao usuário

--- src/main/java/com/ascan/rabbitdockerascan/model/User.java
+++ src/main/java/com/ascan/rabbitdockerascan/model/User.java
@@ -1,21 +1,30 @@
 package com.ascan.rabbitdockerascan.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class User implements Serializable {
 
+    private static final Integer serialVersionUID = 1;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
 
     private String full_name;
-    private LocalDateTime created_at;
+
+    @CreationTimestamp
+    private Instant created_at;
 
     public Integer getId() {
         return id;
@@ -33,11 +42,11 @@ public class User implements Serializable {
         this.full_name = full_name;
     }
 
-    public LocalDateTime getCreated_at() {
+    public Instant getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(LocalDateTime created_at) {
+    public void setCreated_at(Instant created_at) {
         this.created_at = created_at;
     }
 }
